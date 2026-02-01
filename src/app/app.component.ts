b54d360b234bbdc4c0b9ff125ddebd5eb0c3d95d
Yaman <aimane.gaillet@epitech.eu>
2026-02-01 01:11:00 +0100

    feat: component card & input info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { PlayingCardComponent } from "./components/playing-card/playing-card.component";
+import { Monster } from './models/monster.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: "",
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [PlayingCardComponent],
 
 })
 export class AppComponent {
+  monster1!: Monster;
+
+  constructor() {
+    this.monster1 = new Monster();
+    this.monster1.name = "Draco";
+    this.monster1.hp = 40;
+    this.monster1.figureCaption = "N°002";
+    this.monster1.attackDescription = "";
+  }
+
 }
---
+++ src/app/models/monster.model.ts
@@ -0,0 +1,9 @@
+export class Monster {
+    name: string = "My monster";
+    hp: number = 40;
+    figureCaption: string = "N°001 Monster";
+    attackName: string = "Surf";
+    attackStrength: number = 60;
+    attackDescription: string = "Vague deferlante de grande ampleur. Amplitude aléatoire. Augmente avec l'expérience";
+    
+}
