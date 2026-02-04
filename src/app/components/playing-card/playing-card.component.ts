71c7d0f973e4f6694af59ffe3c966433ce3133b7
Yaman <aimane.gaillet@epitech.eu>
2026-02-04 20:51:51 +0100

    done signal input outout

--- src/app/components/playing-card/playing-card.component.ts
+++ src/app/components/playing-card/playing-card.component.ts
@@ -1,5 +1,5 @@
 import { MonsterType, MonsterTypeProperties } from './../../utils/monster.utils';
-import { Component, input, Input, InputSignal, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, computed, input, Input, InputSignal, OnChanges, SimpleChanges } from '@angular/core';
 import { Monster } from '../../models/monster.model';
 
 @Component({
@@ -9,18 +9,22 @@ import { Monster } from '../../models/monster.model';
   templateUrl: './playing-card.component.html',
   styleUrl: './playing-card.component.css'
 })
-export class PlayingCardComponent implements OnChanges {
+export class PlayingCardComponent {
 
-  @Input() monster = new Monster();
-  monsterTypeIcon: string = '../../../assets/img/electrik.jpg';
-  backgroundColor: string = "rgb(255, 255, 104)";
+  monster = input(new Monster()); 
+  monsterTypeIcon = computed(() => {
+    return MonsterTypeProperties[this.monster().type].imageUrl;
+  });
+  backgroundColor = computed(() => {
+    return MonsterTypeProperties[this.monster().type].color;
+  });
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['monster']) {
-      if (changes['monster'].previousValue?.type != changes['monster'].currentValue.type) {
-        this.monsterTypeIcon = MonsterTypeProperties[this.monster.type].imageUrl;
-        this.backgroundColor = MonsterTypeProperties[this.monster.type].color;
-      }
-    }
-  }
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   if (changes['monster']) {
+  //     if (changes['monster'].previousValue?.type != changes['monster'].currentValue.type) {
+  //       this.monsterTypeIcon = MonsterTypeProperties[this.monster().type].imageUrl;
+  //       this.backgroundColor = MonsterTypeProperties[this.monster().type].color;
+  //     }
+  //   }
+  // }
 }
